9ca5f08d1952fc787cea7561a6d64ccfcf215258
Karine Samyn <Karine.Samyn@hogent.be>
2020-12-06 19:43:22 +0100

    Add Display annotations for Product/Index

--- SportsStore/Models/Domain/Availability.cs
+++ SportsStore/Models/Domain/Availability.cs
@@ -1,7 +1,14 @@
-﻿namespace SportsStore.Models.Domain {
-    public enum Availability {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsStore.Models.Domain
+{
+    public enum Availability
+    {
+        [Display(Name = "Shop and online")]
         ShopAndOnline,
+        [Display(Name = "Shop only")]
         ShopOnly,
+        [Display(Name = "Online only")]
         OnlineOnly
     }
 }
